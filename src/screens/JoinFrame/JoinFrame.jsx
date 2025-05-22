baefdba7f09a25837918f45513631630d17d15c5
PHUPAKORN LUEHAN <42826315+Fakecost@users.noreply.github.com>
2025-05-22 07:05:47 +0000

    Update Join

--- src/screens/JoinFrame/JoinFrame.jsx
+++ src/screens/JoinFrame/JoinFrame.jsx
@@ -7,12 +7,12 @@ export const JoinFrame = ({ onClose, onNext }) => {
     <div className="join-frame">
       <div className="div-4">
         <div
-          className="close-button-3"
+          className="close-button-4"
           onClick={onClose}
           style={{ cursor: "pointer" }}
         />
 
-        <div className="overlap-group-5">
+        <div className="overlap-group-7">
           <div className="status-UI-3">
             <div className="main-button-4">
               <img
@@ -32,16 +32,15 @@ export const JoinFrame = ({ onClose, onNext }) => {
               />
             </div>
           </div>
-
           <p className="customer-title">Choose your character as Customer</p>
         </div>
 
         <div className="customer-grid">
-          <div className="container-2">
+          <div className="container-3">
             <CustomerButtonWrapper className="customer-button-instance" />
             {[...Array(23)].map((_, i) => (
               <CustomerButtonWrapper
-                key={i + 2}
+                key={i}
                 className={`customer-button-${i + 2}`}
                 customerIcon={`https://cdn.animaapp.com/projects/682af909abc7ae9309e7e566/releases/682dd5f99fc6c14743fad6a5/img/customericon${
                   i < 2 ? `-${i + 1}` : ""
@@ -56,7 +55,7 @@ export const JoinFrame = ({ onClose, onNext }) => {
           onClick={onNext}
           style={{ cursor: "pointer" }}
         >
-          <div className="text-wrapper-4">Next</div>
+          <div className="text-wrapper-5">Next</div>
         </button>
       </div>
     </div>
