19af7d879c8488f16d0e6ce048ee374d0e812b32
PHUPAKORN LUEHAN <42826315+Fakecost@users.noreply.github.com>
2025-05-28 09:33:59 +0000

    Update Name

--- src/components/QueueItem/QueueItem.jsx
+++ src/components/QueueItem/QueueItem.jsx
@@ -1,5 +1,51 @@
 import React from "react";
 import "./style.css";
+import { availableSkins } from "../../Data";
+
+const allImages = import.meta.glob("/src/PNG-*/**/*.{png,jpg,webp}", {
+  eager: true,
+});
+
+const getImage = (path) =>
+  Object.entries(allImages).find(([k]) => k.endsWith(path))?.[1]?.default || "";
+
+const getIndexFromCharacterName = (name) => {
+  if (typeof name !== "string") return 0;
+
+  const normalizedName = name.trim().toLowerCase();
+
+  const found = availableSkins.find((skin) => {
+    const skinId = (skin.id || "").trim().toLowerCase();
+ return skinId === normalizedName;
+  });
+
+
+  return found?.spriteIndex + 1 ?? 0;
+};
+const getCustomerSpriteStyle = (index) => {
+  const SPRITE_PATH = "/src/PNG-Customer/CustomerIconSpriteSheet.png";
+  const SPRITE_WIDTH = 60;
+  const SPRITE_HEIGHT = 60;
+  const COLUMNS = 8;
+  const x = (index % COLUMNS) * SPRITE_WIDTH;
+  const y = Math.floor(index / COLUMNS) * SPRITE_HEIGHT;
+  const spriteURL = getImage(SPRITE_PATH);
+
+  return {
+    backgroundImage: `url(${spriteURL})`,
+    backgroundPosition: `-${x}px -${y}px`,
+    backgroundSize: `${COLUMNS * SPRITE_WIDTH}px auto`,
+    backgroundRepeat: "no-repeat",
+    imageRendering: "pixelated",
+    width: `${SPRITE_WIDTH}px`,
+    height: `${SPRITE_HEIGHT}px`,
+    // transform: "scale(0.48)",
+    // transformOrigin: "top left",
+    // position: "absolute",
+    // top: "-10px",
+    // left: "0px",
+  };
+};
 
 export const QueueItem = ({
   userName = "Unknown",
@@ -11,11 +57,10 @@ export const QueueItem = ({
     <div className="queue-item">
       <div className="number">{index + 1}</div>
 
-      <img
+      <div
         className="queue-icon"
-        alt={`${characterName}`}
-        src={`/img/${characterName.toLowerCase()}.png`}
-        onError={(e) => (e.target.src = "/img/default.png")}
+        style={getCustomerSpriteStyle(getIndexFromCharacterName(characterName))}
+        aria-label={characterName}
       />
 
       <div className="queue-detail">
